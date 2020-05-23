44280c5b15be9f28b14c464c60482b286a0bda32
a1573595 <a1573595@gmail.com>
2020-05-23 08:50:51 +0800

    Create project & create activity layout

---
+++ app/src/main/java/com/a1573595/musicplayer/playSong/PlaySongActivity.kt
@@ -0,0 +1,13 @@
+package com.a1573595.musicplayer.playSong
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a1573595.musicplayer.R
+
+class PlaySongActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_song)
+    }
+}
---
+++ app/src/main/java/com/a1573595/musicplayer/songList/SongListActivity.kt
@@ -0,0 +1,13 @@
+package com.a1573595.musicplayer.songList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a1573595.musicplayer.R
+
+class SongListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+    }
+}
